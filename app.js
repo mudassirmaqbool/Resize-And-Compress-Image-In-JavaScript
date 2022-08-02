746b3f78570ddde728f36c3c4d764cadb23d0b04
Mudassir Maqbool <>
2022-08-02 14:46:31 +0500

    Send To Server

--- app.js
+++ app.js
@@ -30,11 +30,58 @@ input.addEventListener("change", (event) => {
             context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
             let new_image_url = canvas.toDataURL('image/jpeg', 98)
-            let new_image = document.createElement('img');
-            new_image.src = new_image_url;
-            document.getElementById("wrapper").appendChild(new_image);
+
+            // console.log("Image URL: ", new_image_url)
+
+            let image_file = urlToFile(new_image_url)
+            uploadImage(image_file)
+
+            
+            
+
         } 
 
     }
 })
+
+let urlToFile = (url) => {
+
+    let arr = url.split(",")
+    // console.log(arr)
+    let mime = arr[0].match(/:(.*?);/)[1]
+    let data = arr[1]
+
+    let dataStr = atob(data)
+    let n = dataStr.length
+    let dataArr = new Uint8Array(n)
+
+    while(n--)
+    {
+        dataArr[n] = dataStr.charCodeAt(n)
+    }
+
+    let file  = new File([dataArr], 'File.jpg', {type: mime})
+
+    return file
+
+
+
+}
+
+let uploadImage = (file) => {
+    // upload to server
+    const url = "http://filemanager.test/api/upload"
+
+    let payload = new FormData()
+    payload.append('file', file)
+
+
+    fetch(url, {
+        method: 'POST',
+        body: payload
+    })
+
+
+
+}
 
